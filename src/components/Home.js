cc22bbf168d1d14b00ce3836cdffebecf6470fd7
Leonardo <leo.msouza1706@gmail.com>
2024-09-19 20:03:11 -0300

    Adição página inicial

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import fundo from "../images/Home.png"
+import logo from "../images/Logo beyond.png"
+
+function Home(){
+    return(
+        <div className="size-screen">
+            <img src={fundo} className="w-full h-full absolute z-[-1]" alt="imagem de fundo"/>
+            <div className="flex flex-col items-start ml-12">
+                <img src={logo} alt="logo beyond" className="w-44 ml-[-30px] mt-20 mb-64 "/>
+                <h1 className="text-white text-8xl font-bold mb-5">BeyondForm</h1>
+                <p className="text-cinza text-3xl font-semibold mb-8">Transforme sua presença online conosco</p>
+                <button className="bg-cinza text-white w-36 h-16 text-lg leading-[17px]">Entrar em <br/>contato</button>
+            </div>
+        </div>
+    )
+}
+
+export default Home
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,10 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors:{
+      cinza: '#9b9b9b',
+      white:'#fff'
+    },
     extend: {},
   },
   plugins: [],
